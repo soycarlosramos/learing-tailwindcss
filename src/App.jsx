d1643c8a1dbdb533867ed100889ab9bf99b325e9
Carlos Mario Ramos Perez <carlosmarioramos34@gmail.com>
2023-01-13 18:16:29 -0500

    text

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+	return (
+		<>
+			<div>
+				<p className="text-red-500">Texto 1</p>
+				<p className="text-orange-500">Texto 2</p>
+				<p className="text-yellow-500">Texto 3</p>
+				<p className="text-green-500">Texto 4</p>
+				<p className="text-blue-500">Texto 5</p>
+				<p className="text-violet-500">Texto 6</p>
+			</div>
+		</>
+	)
+}
+
+export default App
